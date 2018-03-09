22f754c7acf721319f99e01891c7e29f06665fa4
Seth Feder <sethfeder2017@u.northwestern.edu>
2018-03-09 00:38:08 -0600

    consolidated files

---
+++ bamazonCustomer.js
@@ -0,0 +1,111 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var cTable = require('console.table');
+var colors = require('colors');
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    afterConnection();
+
+});
+
+function afterConnection() {
+    console.log('\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\nWelcome to Bamazon Department Store\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n\n'.blue + 'Below you can find all our products and their prices!\n'.yellow)
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        productSelect();
+    });
+};
+
+function productSelect() {
+    connection.query("SELECT product_name, item_id, price, stock_quantity FROM products", function (err, results) {
+        if (err) throw err;
+        var choiceArray = [];
+        inquirer.prompt([
+            {
+                name: "productSelect",
+                type: "list",
+                choices: function () {
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].product_name + " | $" + results[i].price);
+                    }
+                    return choiceArray;
+                    console.log("");
+
+                },
+                message: "Which product would you like to purchase? (Use arrow keys to select)",
+                pageSize: 20,
+            },
+        ])
+        .then(function (answer) {
+            // get the information of the chosen item
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (choiceArray.indexOf(answer.productSelect) + 1 === results[i].item_id) {
+                    chosenItem = results[i];
+                }
+            }
+            inquirer.prompt([
+                {
+                    name: "productUnits",
+                    type: "input",
+                    message: "How many of those would you like to buy, hun?",
+                    validate: function (value) {
+                        if (isNaN(value) === false && value <= chosenItem.stock_quantity) {
+                            return true;
+                        } else if (isNaN(value) === false && value > chosenItem.stock_quantity) {
+                            console.log("\n\nI can't give ya that many, babe! We only have ".red + chosenItem.stock_quantity + " left in stock.\n".red);
+                            return false;
+                        } else {
+                            console.log("\n\nWhat kinda number is that, darlin'?!\n".red)
+                            return false;
+
+                        }
+                    }
+                },
+
+            ])
+            .then(function (answer) {
+
+                connection.query("UPDATE products SET stock_quantity =" + (chosenItem.stock_quantity - parseInt(answer.productUnits)) + " WHERE item_id =" + chosenItem.item_id, function (err, res) {
+                    if (err) throw err;
+
+                })
+                console.log("\nComin' right up, hun! That's gonna run you $" + answer.productUnits * chosenItem.price + "."+"\n\nPurchase Successfull!!\n".green)
+
+                inquirer.prompt([
+                    {
+                        name: "anotherPurchase",
+                        type: "confirm",
+                        message: "Would you like to make another purchase?",
+
+                    },
+                ])
+                .then(function (answer) {
+                    if (answer.anotherPurchase === true) {
+                        productSelect();
+                    } else {
+                        console.log("\nAlright then, dear. Thanks for shoppin'. You have yourself a good one!\n")
+                        connection.end();
+                    }
+
+                })
+            })
+        })
+    })
+}
---
+++ bamazonManager.js
@@ -0,0 +1,303 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var cTable = require('console.table');
+var colors = require('colors');
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    console.log('\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\nWelcome to Bamazon Manager View\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n'.blue)
+    inquirer.prompt([
+        {
+            name: "managerSelect",
+            type: "list",
+            choices: ["View products currently for sale", "View products with low inventory", "Restock inventory", "Add a new product to sell", "Exit manager view"],
+            message: "What would you like to do, Manager? (Use arrow keys to select)",
+            pageSize: 10,
+        },
+    ])
+    .then(function (answer) {
+        if (answer.managerSelect === "View products currently for sale") {
+            viewProducts();
+        } else if (answer.managerSelect === "View products with low inventory") {
+            viewInventory();
+        } else if (answer.managerSelect === "Restock inventory") {
+            restockInventory();
+        } else if (answer.managerSelect === "Add a new product to sell") {
+            addNewProduct();
+        } else {
+            console.log("\nAlright then, Manager. Thanks for stopin' by. You have yourself a good one!\n")
+            connection.end()
+        }
+        
+    });
+};
+
+function viewProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err
+        console.log("");
+        console.table(res);
+        console.log("");
+        inquirer.prompt([
+            {
+                name: "anythingElse",
+                type: "confirm",
+                message: "Would you like to do anything else?"
+            },
+        ])
+        .then(function (answer) {
+            if (answer.anythingElse === true) {
+                start();
+            } else {
+                console.log("\nAlright then, Manager. Thanks for stopin' by. You have yourself a good one!\n")
+                connection.end();
+            }
+        })
+    })
+}
+
+function viewInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity<" + 5, function (err, res) {
+        if (err) throw err
+
+        if (res[0] === undefined) {
+            console.log("\nLooks like you're in good shape! You don't have any products with an inventory lower than 5!\n".green);
+            inquirer.prompt([
+                {
+                    name: "anythingElse",
+                    type: "confirm",
+                    message: "Would you like to do anything else?"
+                },
+            ])
+            .then(function (answer) {
+                if (answer.anythingElse === true) {
+                    start();
+                } else {
+                    console.log("\nAlright then, Manager. Thanks for stopin' by. You have yourself a good one!\n")
+                    connection.end();
+                }
+            })
+                    
+        } else {
+            console.log("\n You'll need to restock these products soon:\n");
+            console.table(res);
+            console.log("");
+            inquirer.prompt([
+                {
+                    name: "anythingElse",
+                    type: "confirm",
+                    message: "Would you like to do anything else?"
+                },
+            ])
+            .then(function (answer) {
+                if (answer.anythingElse === true) {
+                    start();
+                } else {
+                    console.log("\nAlright then, Manager. Thanks for stopin' by. You have yourself a good one!\n")
+                    connection.end();
+                }
+            })
+        }
+    })
+}
+
+function restockInventory() {
+    connection.query("SELECT product_name, item_id, price, stock_quantity FROM products", function (err, results) {
+        if (err) throw err;
+        var choiceArray = [];
+        inquirer.prompt([
+            {
+                name: "productSelect",
+                type: "list",
+                choices: function () {
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].product_name + " | " + results[i].stock_quantity);
+                    }
+                    return choiceArray;
+
+                },
+                message: "Which product would you like to restock? (Use arrow keys to select)",
+                pageSize: 20,
+            },
+        ])
+        .then(function (answer) {
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (choiceArray.indexOf(answer.productSelect) + 1 === results[i].item_id) {
+                    chosenItem = results[i];
+                }
+            }
+            inquirer.prompt([
+                {
+                    name: "productUnits",
+                    type: "input",
+                    message: "Restock with what quantity?",
+                    validate: function (value) {
+                        if (isNaN(value) === false && value < 1000 && value > 0) {
+                            return true;
+                        } else if (isNaN(value) === false && value > 999 && value > 0) {
+                            console.log("\n\nCome on now, we can't buy that many! Pick a reasonable number less than 1000\n".red);
+                            return false;
+                        } else if (isNaN(value) === false && value < 1000 && value < 1) {
+                            console.log("\n\nYou can't restock with nothing or negative amounts!\n".red)
+                            return false;
+                        } else {
+                            console.log("\n\nWhat kinda number is that, Sir?!\n".red)
+                            return false;
+
+                        }
+                    }
+                },
+
+            ])
+            .then(function (answer) {
+
+                connection.query("UPDATE products SET stock_quantity =" + (chosenItem.stock_quantity + parseInt(answer.productUnits)) + " WHERE item_id =" + chosenItem.item_id, function (err, res) {
+                    if (err) throw err;
+
+                })
+                console.log("\nAlright, we'll get right on that.\n You now have " + (chosenItem.stock_quantity + parseInt(answer.productUnits)) + " of those!"+"\n\nRestock Successfull!!\n".green)
+
+                inquirer.prompt([
+                    {
+                        name: "anotherRestock",
+                        type: "confirm",
+                        message: "Would you like to restock anything else?",
+
+                    },
+                ])
+                .then(function (answer) {
+                    if (answer.anotherRestock === true) {
+                        restockInventory();
+                    } else {
+                        inquirer.prompt([
+                            {
+                                name: "anythingElse",
+                                type: "confirm",
+                                message: "Would you like to do anything else?"
+                            },
+                        ])
+                        .then(function (answer) {
+                            if (answer.anythingElse === true) {
+                                start();
+                            } else {
+                                console.log("\nAlright then, Manager. Thanks for stopin' by. You have yourself a good one!\n")
+                                connection.end();
+                            }
+                        })
+                    }
+                })
+            })
+        })
+    })
+}
+
+function addNewProduct() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What new product would you like to add?",
+        },
+        {
+            name: "department",
+            type: "input",
+            message: "What department will sell that product?",
+        },
+        {
+            name: "price",
+            type: "input",
+            message: "How much should it cost?",
+            validate: function (value) {
+                if (isNaN(value) === false && value < 1000000 && value > 0) {
+                    return true;
+                } else if (isNaN(value) === false && value > 999999 && value > 0) {
+                    console.log("\n\nCome on now, we can't sell them for that much! Pick a reasonable number less than $1,000,000.\n".red);
+                    return false;
+                } else if (isNaN(value) === false && value < 1000000 && value < 1) {
+                    console.log("\n\nYou can't charge people nothing or negative amounts. We need to make a profit!\n".red)
+                    return false;
+                } else {
+                    console.log("\n\nWhat kinda number is that, Sir?!\n".red)
+                    return false;
+
+                }
+            }
+        },
+        {
+            name: "stock",
+            type: "input",
+            message: "How many would you like to start out with in your inventory?",
+            validate: function (value) {
+                if (isNaN(value) === false && value < 1000 && value > 0) {
+                    return true;
+                } else if (isNaN(value) === false && value > 999 && value > 0) {
+                    console.log("\n\nCome on now, we can't buy that many! Pick a reasonable number less than 1000.\n".red);
+                    return false;
+                } else if (isNaN(value) === false && value < 1000 && value < 1) {
+                    console.log("\n\nYou can't stock your inventory with nothing or negative amounts!\n".red)
+                    return false;
+                } else {
+                    console.log("\n\nWhat kinda number is that, Sir?!\n".red)
+                    return false;
+
+                }
+            }
+        },
+    ])
+    .then(function (answer) {
+        connection.query("INSERT into products (product_name, department_name, price, stock_quantity) VALUES ('" + answer.name + "', '" + answer.department + "', " + answer.price + ", " + answer.stock + ")", function (err, results) {
+            if (err) throw err;
+            console.log('\nYou successfully added '.green + answer.name + ' to your inventory!!\n'.green)
+
+            inquirer.prompt([
+                {
+                    name: "anotherProduct",
+                    type: "confirm",
+                    message: "Would you like to add another new product?",
+
+                },
+            ])
+            .then(function (answer) {
+                if (answer.anotherProduct === true) {
+                    addNewProduct();
+                } else {
+                    inquirer.prompt([
+                        {
+                            name: "anythingElse",
+                            type: "confirm",
+                            message: "Would you like to do anything else?"
+                        },
+                    ])
+                    .then(function (answer) {
+                        if (answer.anythingElse === true) {
+                            start();
+                        } else {
+                            console.log("\nAlright then, Manager. Thanks for stopin' by. You have yourself a good one!\n")
+                            connection.end();
+                        }
+                    })
+                }
+            })
+        })
+    })
+}
+
+
